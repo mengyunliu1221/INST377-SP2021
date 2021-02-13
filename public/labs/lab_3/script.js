3f4f71d9347ef35105ad259f2bfa8a05e2d13fe3
mengyunliu1221 <57872197+mengyunliu1221@users.noreply.github.com>
2021-02-13 13:09:45 +0800

    lab3

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,23 @@
 /* Put your javascript in here */
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const imgs = document.getElementsByTagName("img");
+let arr = ["images/onigiri_1.png","images/onigiri_2.png","images/onigiri_3.png","images/onigiri_4.png","images/roll_1.png","images/roll_2.png","images/roll_3.png"]; 
+left.onclick = function(){
+    let a =  arr.pop()
+    arr.unshift(a)
+    for(let i = 0; i < imgs.length; i++){
+        imgs[i].src = arr[i];
+    }
+    return true;
+}
+right.onclick = function(){
+    let s = arr.shift()
+    arr.push(s); 
+    for(let i = 0; i < imgs.length; i++){
+        imgs[i].src = arr[i];
+    }
+    return true;
+}
+
+
